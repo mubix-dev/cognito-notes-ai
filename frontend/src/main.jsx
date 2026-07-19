393e8cc8d73b52937cf646d96758a4e66e464b18
Mubeen Khan <mubix.dev@gmail.com>
2026-07-20 00:48:08 +0500

    feat: expose server URL from environment variables

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+export const serverURL = import.meta.env.VITE_BACKEND_URL
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
