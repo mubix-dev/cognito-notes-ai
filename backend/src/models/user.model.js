9ea7f43ce8760b9de0136b9b8c7aec0c6765b7d2
Mubeen Khan <mubix.dev@gmail.com>
2026-07-19 21:47:06 +0500

    Add user model with schema definition for user data

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    credits:{
+        type:Number,
+        default:50,
+        min:0
+    },
+    isCreditAvailable:{
+        type:Boolean,
+        default:true
+    },
+    notes:{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:"Notes",
+        default:[]
+    }
+},{timestamps:true})
+
+
+const User = mongoose.model("User",userSchema)
+export default User
