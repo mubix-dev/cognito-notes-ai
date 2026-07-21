ede3ccef7f8b5de98365d21cad67dac8991884ab
Mubeen Khan <mubix.dev@gmail.com>
2026-07-21 21:29:01 +0500

    feat: add route for retrieving user's notes

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { generateNotes } from "../controllers/notes.controller.js";
+import { generateNotes, getMyNotes } from "../controllers/notes.controller.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const router = Router();
 
 router.post("/generate", isAuth, generateNotes);
+router.get("/my-notes", isAuth, getMyNotes);
 
 export default router;
