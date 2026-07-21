f7a2e9f4ae8c5248d2396bb7151ba8b8280ae5a0
Mubeen Khan <mubix.dev@gmail.com>
2026-07-21 21:43:37 +0500

    feat: add deleteNote route to enable note deletion functionality

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { generateNotes, getMyNotes } from "../controllers/notes.controller.js";
+import { generateNotes, getMyNotes, deleteNote } from "../controllers/notes.controller.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const router = Router();
 
 router.post("/generate", isAuth, generateNotes);
 router.get("/my-notes", isAuth, getMyNotes);
+router.delete("/:id", isAuth, deleteNote);
 
 export default router;
