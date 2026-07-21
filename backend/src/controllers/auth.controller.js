71eacbafdda4fbd79cfee0776998dc4d68854655
Mubeen Khan <mubix.dev@gmail.com>
2026-07-21 22:22:29 +0500

    feat: update SameSite attribute for cookie based on environment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -22,7 +22,7 @@ const googleAuth = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
